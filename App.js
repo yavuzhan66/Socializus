b4572850811e33239faabf0b8821d0ddcc6e7507
Yavuzhan <99103025+yavuzhan66@users.noreply.github.com>
2023-11-28 15:59:07 +0100

    app

--- App.js
+++ App.js
@@ -1,59 +1,21 @@
-import React, { useState } from "react";
-import { StyleSheet, Text, View, Button, FlatList } from "react-native";
+// Exercice 1
+import React from "react";
+import { View } from "react-native";
 
 export default function App() {
+
   const obj = [
-    {},
-    { id: 1, name: "Stan", age: 45 },
-    { id: 2, name: "Francine", age: 40 },
-    { id: 3, name: "Hashley", age: 35 },
-    { id: 4, name: "Sophie", age: 25 },
-    { id: 5, name: "Natasha", age: 15 },
-    { id: 6, name: "Ophélie", age: 28 },
-  ];
-  const [family, setFamily] = useState(obj);
+    {id: "1", name: "Stan", age: 45},
+    {id: "2", name: "Francine", age: 45},
+    {id: "3", name: "hayley", age: 18},
+    {id: "4", name: "Steve", age: 14},
+    {id: "5", name: "Roger", age: 1020},
+    {id: "6", name: "Klaus", age: 30},
+  ]
 
   return (
-    <View style={styles.wrapper}>
-        {
-        family.map(member => {
-          return (
-            <View key={member.id} style={styles.viewList}>
-              <Text style={styles.text}>
-                <Text style={styles.textBold}> Nom: </Text>
-                {member.name}
-              </Text>
-
-              <Text style={styles.text}>
-                <Text style={styles.textBold}> Age: </Text>
-                {member.age}
-              </Text>
-            </View>
-          );
-        })}
-      
-    </View>
+    <View></View>
   );
 }
 
-
-
-
-const styles = StyleSheet.create({
-  wrapper: {
-    padding: 20,
-  },
-    viewList: {
-      marginTop: 30,
-      backgroundColor: "purple",
-      padding: 19
-    },
-    text: {
-      color: "#fff",
-      fontSize: 20
-    },
-    textBold: {
-      fontWeight: "Bold"
-    },
-  
-});
+const styles = StyleSheet.create({});
